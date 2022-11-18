a6700ef29868380e1f74ef3ba31240bf4ceca496
Christian <gonzalo.espinozacadillo@gmail.com>
2022-11-18 10:20:06 -0500

    add method created

--- src/02-methods.ts
+++ src/02-methods.ts
@@ -12,6 +12,18 @@ export class MyDate {
     printFormat() {
         return `${this.date}/${this.month}/${this.year}`
     }
+
+    add(amount: number, type: 'days' | 'month' | 'years') {
+        if (type === 'days') {
+            this.date += amount
+        }
+        if (type === 'month') {
+            this.month += amount
+        }
+        if (type === 'years') {
+            this.year += amount
+        }
+    }
 }
 
 const myDate = new MyDate(1998,10,2)

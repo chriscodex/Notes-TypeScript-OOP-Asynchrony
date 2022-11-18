2ab1e97765239142d252cb23924e1e1d09f1411e
Christian <gonzalo.espinozacadillo@gmail.com>
2022-11-18 10:12:56 -0500

    class myDate created in methods

---
+++ src/02-methods.ts
@@ -0,0 +1,13 @@
+export class MyDate {
+    year: number
+    month: number
+    date: number
+
+    constructor(year: number, month: number, date: number) {
+        this.year = year
+        this.month = month
+        this.date = date
+    }
+
+
+}

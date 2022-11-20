2a360515662d0581f0fbd6e79dd897d6d8d19c50
Christian <gonzalo.espinozacadillo@gmail.com>
2022-11-20 13:59:58 -0500

    myDate class constructor created

--- src/03-public.ts
+++ src/03-public.ts
@@ -3,4 +3,9 @@ export class MyDate {
     month: number
     year: number
 
+    constructor(date: number, month: number, year: number) {
+        this.date = date
+        this.month = month
+        this.year = year
+    }
 }

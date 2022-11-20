ba107ea0c0f769455fda8c4e19c485ba189f4941
Christian <gonzalo.espinozacadillo@gmail.com>
2022-11-20 14:04:38 -0500

    myDate class - method add created

--- src/03-public.ts
+++ src/03-public.ts
@@ -4,11 +4,26 @@ export class MyDate {
     month: number
     year: number
 
-    constructor(date: number, month: number, year: number) {
+    constructor(year: number, month: number, date: number) {
         this.date = date
         this.month = month
         this.year = year
     }
 
-
+    /*  */
+    add(amount: number, type: 'year' | 'month' | 'date') {
+        if (type === 'year') {
+            this.year += amount
+        }
+        if (type === 'month') {
+            this.month += amount
+        }
+        if (type === 'date') {
+            this.date += amount
+        }
+    }
 }
+
+const myDate = new MyDate(1998,10,2)
+
+

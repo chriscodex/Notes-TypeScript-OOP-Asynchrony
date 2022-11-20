bb9e4f3f60e6ea390a19c1f994fb01b824ef218c
Christian <gonzalo.espinozacadillo@gmail.com>
2022-11-20 14:12:15 -0500

    class Mydate created in private.ts file

---
+++ src/04-private.ts
@@ -0,0 +1,11 @@
+export class MyDate {
+    date: number
+    month: number
+    year: number
+
+    constructor(year: number, month: number, date: number) {
+        this.year = year
+        this.month = month
+        this.date = date
+    }
+}

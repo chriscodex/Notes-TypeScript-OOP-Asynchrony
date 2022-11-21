0d74bc7156ef24e4a2a08c3a1c6d1a97f92d4c03
Christian <gonzalo.espinozacadillo@gmail.com>
2022-11-21 14:19:50 -0500

    add method now is private

--- src/04-private.ts
+++ src/04-private.ts
@@ -9,7 +9,7 @@ export class MyDate {
         this.date = date
     }
 
-    addPadding(value: number) {
+    private addPadding(value: number) {
         if (value < 10) {
             return `0${value}`
         } else {

1b207016954aa9d8cc8ef88fb118ab48992c92e8
Christian <c.espinoza.code@gmail.com>
2022-11-25 01:13:36 -0500

    async function created

--- src/14-promises.ts
+++ src/14-promises.ts
@@ -11,7 +11,27 @@ import axios from 'axios'
         })
         return promise
     }
+
+    function getProducts() {
+        const promise = axios.get('https://api.escuelajs.co/api/v1/products')
+        return promise
+    }
+
+    async function getProductsAsync() {
+        const promise = await axios.get('https://api.escuelajs.co/api/v1/products')
+        return promise.data
+    }
+
     console.log('---'.repeat(10))
     const rta = await delay(2000)
     console.log(rta)
+
+    console.log('---'.repeat(10))
+    const products = await getProducts()
+    console.log(products.data)
+
+    console.log('---'.repeat(10))
+    const productsV2 = await getProductsAsync()
+    console.log(productsV2)
+
 })()

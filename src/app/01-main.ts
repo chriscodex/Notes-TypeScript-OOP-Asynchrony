20d9d395da077e2ecd0381845a087b3d29f36e8f
Christian <c.espinoza.code@gmail.com>
2022-11-26 10:46:26 -0500

    comment about the type added

--- src/app/01-main.ts
+++ src/app/01-main.ts
@@ -2,8 +2,21 @@ import axios from 'axios'
 import { Product } from './models/product.model'
 
 (async ()=>{
+    /* Tipado desde el retorno:
+    async function getProduct(): Product[]
+    */
     async function getProducts() {
-        const promise = await axios.get<string[]>('https://api.escuelajs.co/api/v1/products')
+        /* Tipado desde la librería
+        Con la expresión Product[] se ha tipado la promesa tanto desde dentro como fuera */
+        const promise = await axios.get<Product[]>('https://api.escuelajs.co/api/v1/products')
+
+        /* Tipado desde el cast
+        En caso la librería no soporte esta expresión, se puede usar:
+        const rta = await axios.get('https://api.escuelajs.co/api/v1/products')
+        const data = rta.data as Product[]
+        return data
+        */
+
         return promise.data
     }
 

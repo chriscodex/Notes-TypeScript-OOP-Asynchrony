cb3a88ad449513421062384f0a07912fd2aa8c66
Christian <c.espinoza.code@gmail.com>
2022-11-26 10:59:39 -0500

    products dtos and services finished

--- src/app/dtos/product.dto.ts
+++ src/app/dtos/product.dto.ts
@@ -1,7 +1,8 @@
 import { Product } from "./../models/product.model";
+import { Category } from "../models/category.model";
 
 export interface CreateProductDto extends Omit<Product, 'id' | 'category'> {
-    categoryId: string
+    categoryId: Category['id']
 }
 
 export interface UpdateProductDto extends Partial<CreateProductDto> {

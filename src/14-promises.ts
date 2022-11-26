5c37ca2551f3d0ea25171ea660d6412382fcc01a
Christian <c.espinoza.code@gmail.com>
2022-11-25 20:03:42 -0500

    comment of promise added

--- src/14-promises.ts
+++ src/14-promises.ts
@@ -1,14 +1,15 @@
 (async ()=>{
 
     function delay(time: number) {
-        const promise = new Promise((resolve)=>{
+        /* This promise return a boolean */
+        const promise = new Promise<boolean>((resolve)=>{
             setTimeout(()=>{
                 resolve(true)
             })
         })
         return promise
     }
-
+    console.log('---'.repeat(10))
     const rta = await delay(2000)
-
+    console.log(rta)
 })()
